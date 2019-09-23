31186f93c0aac3fe3fc7060fa0384eebe5d49605
Mez Charney <mez.charney@gmail.com>
2019-09-23 23:16:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,9 +20,12 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  if source_array.reduce {|n| n == true}
-  else source_array.reduce {|n| n != true}
+   counter = 0
+  while counter < source_array.length do
+    return false if source_array[counter] == false
+    counter += 1 
   end
+  return true
 end
 
 def reduce_to_any_true(source_array)
