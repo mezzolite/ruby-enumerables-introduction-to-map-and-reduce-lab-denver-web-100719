360c6d529812e3fb34120777dccc48786795304a
Mez Charney <mez.charney@gmail.com>
2019-09-23 22:42:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,11 +16,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  counter = 0 
-  while counter < source_array.length do
-    source_array[counter] += source_array[counter + 1]
-  counter += 1 
-end
+ source_array.reduce(starting_point) { |sum, n| sum + n }
 end
 
 def reduce_to_all_true(source_array)
