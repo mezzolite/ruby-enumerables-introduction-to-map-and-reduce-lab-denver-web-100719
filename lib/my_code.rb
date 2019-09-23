2f90727495f1dca0a3c8919b41fd1820e67db9c0
Mez Charney <mez.charney@gmail.com>
2019-09-23 23:15:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ end
 def reduce_to_any_true(source_array)
   counter = 0
   while counter < source_array.length do
-    
+    return true if source_array[counter] == true
     counter += 1 
   end
   return false
